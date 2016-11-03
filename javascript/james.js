2ed6c5a03b19e2ab673374ecdca914fa8b1c38ae
James Milton <james.milton@me.com>
2016-11-03 21:48:48 +0000

    trying to get more analytics functions

---
+++ javascript/james.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	
+    var x = Math.floor((Math.random() * 2) + 1);
+    
+    
+    if (x == 1) {
+	    //light
+	    document.getElementById('colourHeader').style.color = '#696969';
+	    document.getElementById('colourBack').style.backgroundColor = '#FFFAFA';
+	    
+	    
+	    
+	    
+	    var all = document.getElementsByClassName('colourPara1');
+		for (var i = 0; i < all.length; i++) {
+		  all[i].style.color = '#696969';
+		}
+		
+		var all = document.getElementsByClassName('colourLink');
+		for (var i = 0; i < all.length; i++) {
+		  all[i].style.color = '#808080';
+		}
+
+	} else {
+		//dark
+	    document.getElementById('colourHeader').style.color = '#696969';
+	    document.getElementById('colourBack').style.backgroundColor = '#D3D3D3';
+	    
+		var all = document.getElementsByClassName('colourPara1');
+		for (var i = 0; i < all.length; i++) {
+		  all[i].style.color = '#FFFAFA';
+		}
+		
+		var all = document.getElementsByClassName('colourLink');
+		for (var i = 0; i < all.length; i++) {
+		  all[i].style.color = '#696969';
+		}
+
+
+	} 
+});
