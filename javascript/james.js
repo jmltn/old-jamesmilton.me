de46d684b002f383842c19062bff7ed27da342c4
jmltn <james.milton@me.com>
2017-01-24 22:56:20 +0000

    first commit of dissertation page

--- javascript/james.js
+++ javascript/james.js
@@ -52,19 +52,23 @@ function getPos(e) {
 		
 		
 		var table1 = document.querySelector("#mainSquare1");	
+/*
 		table1.style.webkitTransform = "rotateY("+degX+"deg) rotateX("+degY+"deg)";
 		table1.style.MozTransform = "rotateY("+degX+"deg) rotateX("+degY+"deg)";
 		table1.style.msTransform = "rotateY("+degX+"deg) rotateX("+degY+"deg)";
 		table1.style.OTransform = "rotateY("+degX+"deg) rotateX("+degY+"deg)";
+*/
 		table1.style.transform = "rotateY("+degX+"deg) rotateX("+degY+"deg)";
 		
 		    
 		
 		var table2 = document.querySelector("#mainSquare2");	
+/*
 		table2.style.webkitTransform = "rotateY("+degX+"deg) rotateX("+degY+"deg)";
 		table2.style.MozTransform = "rotateY("+degX+"deg) rotateX("+degY+"deg)";
 		table2.style.msTransform = "rotateY("+degX+"deg) rotateX("+degY+"deg)";
 		table2.style.OTransform = "rotateY("+degX+"deg) rotateX("+degY+"deg)";
+*/
 		table2.style.transform = "rotateY("+degX+"deg) rotateX("+degY+"deg)";
 	}
 }
@@ -146,3 +150,35 @@ $(window).load(function() {
   
 })();
 
+
+
+
+//countdown
+// Set the date we're counting down to
+var countDownDate = new Date("May 1, 2018 12:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("countdown").innerHTML = days + " days " + hours + " hours "
+    + minutes + " minutes and " + seconds + " seconds";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
